dc8d3e7fa740ebb82cbddc1a48a339c82bf2e53d
Bitlatte <github@bitlatte.io>
2022-07-07 23:43:11 -0500

    Update Help Menu

--- help.go
+++ help.go
@@ -7,7 +7,11 @@ import (
 )
 
 func showHelp() {
-	fmt.Println("Usage: shex -f <path_to_shell_file> [host_addresses]")
+	fmt.Println("Usage: beam <options> [host_addresses]")
+	fmt.Println("Example:\n	beam -f <path_to_shell_file> [host_addresses]")
+	fmt.Println("Commands")
+	fmt.Println("	key		Show your ssh key")
+	fmt.Println("Options")
 	flag.PrintDefaults()
 	os.Exit(1)
 }
